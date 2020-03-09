fb3c7ec2cd801b07c6fcac3f5ffd0df61193724c
TranNguyenDuy <duytngcs16168@fpt.edu.vn>
2020-03-09 21:52:21 +0700

    Login function - Biết hướng làm nhưng mà vẫn chưa thao tác được với mongodb

--- routes/admin.js
+++ routes/admin.js
@@ -1,33 +1,38 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const mongoClient = require('mongodb').mongoClient;
+const mongoUrl = 'mongodb://heroku_ptn142g5:fur6mijdqu54vb26q9e7c08qfg@ds141786.mlab.com:41786/heroku_ptn142g5';
+var assert = require('assert');
 
-router.get('/', (req, res, next)=>{
-    res.render('admin/adminDashboard',{dashboard: 'dashboard'})
-})
-//  Tutor 
-router.get('/tutorList', (req, res, next)=>{
-    res.render('admin/manageTutors',{tutor: 'tutor'})
-})
-router.get('/tutor-upload/:id', (req, res, next)=>{
-    res.render('admin/tutorUpload',{tutor: 'tutor'})
-})
-router.get('/tutorDetail/:id', (req, res, next)=>{
-    res.render('admin/tutorDetail',{tutor: 'tutor'})
-})
+/* GET admin pages. */
+router.get('/', (req, res, next) => {
+	res.render('admin/adminDashboard', { dashboard: 'dashboard' });
+});
+//  Tutor
+router.get('/tutorList', (req, res, next) => {
+	res.render('admin/manageTutors', { tutor: 'tutor' });
+});
+router.get('/tutor-upload/:id', (req, res, next) => {
+	res.render('admin/tutorUpload', { tutor: 'tutor' });
+});
+router.get('/tutorDetail/:id', (req, res, next) => {
+	res.render('admin/tutorDetail', { tutor: 'tutor' });
+});
 
 // Student
-router.get('/studentList', (req, res, next)=>{
-    res.render('admin/manageStudent',{student: 'student'})
-})
-router.get('/student-upload/:id', (req, res, next)=>{
-    res.render('admin/studentUpload',{student: 'student'})
-})
-router.get('/studentDetail/:id', (req, res, next)=>{
-    res.render('admin/studentDetail',{tutor: 'tutor'})
-})
+router.get('/studentList', (req, res, next) => {
+	res.render('admin/manageStudent', { student: 'student' });
+});
+router.get('/student-upload/:id', (req, res, next) => {
+	res.render('admin/studentUpload', { student: 'student' });
+});
+router.get('/studentDetail/:id', (req, res, next) => {
+	res.render('admin/studentDetail', { tutor: 'tutor' });
+});
 
 // Course
-router.get('/course', (req, res, next)=>{
-    res.render('admin/manageCourse',{course: 'course'})
-})
-module.exports = router
+router.get('/course', (req, res, next) => {
+	res.render('admin/manageCourse', { course: 'course' });
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+const mongo = require('mongodb');
+const mongoUrl = 'mongodb://heroku_ptn142g5:fur6mijdqu54vb26q9e7c08qfg@ds141786.mlab.com:41786/heroku_ptn142g5';
 
 /* GET users listing. */
 router.get('/signin', function(req, res, next) {
-  res.render('user/signin',{
-    layout: false
-  })
+	res.render('user/signin', {
+		tille: 'login',
+		layout: false
+	});
 });
+
+// log in
+router.post('/signin', function(req, res) {
+	var testEmail = 'Sterling26@hotmail.com';
+	var testPassword = 'HeLqTHbee7XiDJD';
+	mongo.Promise = global.Promise;
+	mongo.connect(mongoUrl, function(err, db) {
+		if (err) throw err;
+		if (req.body.email == testEmail && req.body.password == testPassword) {
+			/* session handle 
+			
+			*/
+
+			res.redirect('/');
+		} else {
+			res.redirect('/users/signin');
+		}
+	});
+});
+
+// log out
+router.post('/signout', function(req, res) {
+	req.session.destroy((err) => {
+		if (err) {
+			throw err;
+		} else {
+			res.redirect('user/sigin');
+		}
+	});
+});
+
 module.exports = router;
